530189676e6610210561795b7027c6f016ce0f0b
Evgeny Bondarenko <evgeny.bondarenko@onetwotrip.com>
2019-09-04 14:46:50 +0300

    add test for error

--- test/index.js
+++ test/index.js
@@ -12,4 +12,7 @@ describe('checkUserEmail', ()=>{
     const res = lib.emailToName('vladimir.putin@mail.ru');
     assert.equal(res, 'Vladimir Putin');
   });
+  it('should throw an error when email is not email', ()=>{
+    assert.throws(()=>lib.emailToName('vladimir.putinmail.ru'), Error);
+  });
 });
